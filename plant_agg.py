1c7f289e000cc24ce58da393fcd89f6e1ee17ff6
Mingze Li <49802799+Mingze-Li128@users.noreply.github.com>
2021-01-05 12:41:03 -0600

    agg

--- plant_agg.py
+++ plant_agg.py
@@ -1,3 +1,6 @@
+
+
+
 import pandas as pd
 import numpy as np
 import networkx as nx
@@ -77,49 +80,62 @@ def Loc_of_plant():
         loc_of_plant[row['NAME']] = loc
     return loc_of_plant
 
-def write_plant(clean_data,ZipOfsub_dict):
+def Plant_agg(clean_data,ZipOfsub_dict,loc_of_plant,LocOfsub_dict,Pmin):
+    plant_dict = {}
+  
+    for index, row in clean_data.iterrows():
+        tu = (row['PLANT'],row['TYPE'])
+        if(tu not in plant_dict):
+            if(row['PLANT'] in Pmin and row['PLANT'] in loc_of_plant):
+                if(row['ZIP'] in ZipOfsub_dict):
+                    min_d = 100000.0
+                    min_s = ""
+                    for value in ZipOfsub_dict[row['ZIP']]:
+                    # calculate the distance between the plant and substations
+                        if(geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m < min_d):
+                            min_s = value
+                            min_d = geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m
+                    bus_id = value
+                    pmin = Pmin[row['PLANT']]
+            # if this zip does not contain subs, we try to find subs in neighbor zip.
+                else: 
+                    zi = int(row['ZIP'])
+                    for i in range(-5,6):
+                        min_d = 100000.0
+                        min_s = ""
+                        if(str(zi+i) in Pmin and str(zi+i) in loc_of_plant):
+                            for value in ZipOfsub_dict[str(zi+i)]:
+                                if(geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m < min_d):
+                                    min_s = value
+                                    min_d = geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m
+                    bus_id = value
+                    pmin = 0
+            pmaxwin = row['WINTER_CAP']
+            pmaxsum = row['SUMMER_CAP']
+            list1 = [bus_id,pmaxwin,pmaxsum,pmin]
+            plant_dict[tu] = list1
+        else:
+            list1 = plant_dict[tu]
+            list1[1] = list1[1] + row['WINTER_CAP']
+            list1[2] = list1[2] + row['SUMMER_CAP']
+            plant_dict[tu] = list1
+    return plant_dict
+
+def write_plant(plant_dict):
     plant = open('plant.csv','w',newline='')
     csv_writer = csv.writer(plant)
     csv_writer.writerow(["plant_id","bus_id","Pg","status","Pmax","Pmin","ramp_30","type"])
-    for index, row in clean_data.iterrows():
-        if(row['PLANT'] in Pmin and row['PLANT'] in loc_of_plant):
-            if(row['ZIP'] in ZipOfsub_dict):
-                min_d = 100000.0
-                min_s = ""
-                for value in ZipOfsub_dict[row['ZIP']]:
-                    # calculate the distance between the plant and substations
-                    if(geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m < min_d):
-                        min_s = value
-                        min_d = geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m
-                bus_id = value
-            # if this zip does not contain subs, we try to find subs in neighbor zip.
-            else: 
-                zi = int(row['ZIP'])
-                for i in range(-5,6):
-                    min_d = 100000.0
-                    min_s = ""
-                    if(str(zi+i) in Pmin and str(zi+i) in loc_of_plant):
-                        for value in ZipOfsub_dict[str(zi+i)]:
-                            if(geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m < min_d):
-                                min_s = value
-                                min_d = geodesic(loc_of_plant[row['PLANT']],LocOfsub_dict[value]).m
-                bus_id = value
-            pmax = min(row['SUMMER_CAP'],row['WINTER_CAP'])
-            pmin = Pmin[row['PLANT']]
-            csv_writer.writerow([row['PLANT']+'-'+row['NAME']+'-'+row['ZIP'],bus_id, 1, "OP", pmax,pmin,pmin,row['TYPE']])
-        else:
-            bus_id = 0
-            pmax = min(row['SUMMER_CAP'],row['WINTER_CAP'])
-            pmin = 0
-            csv_writer.writerow([row['PLANT']+'-'+row['NAME']+'-'+row['ZIP'],bus_id, 1, "OP", pmax,pmin,pmin,row['TYPE']])
+    for key in plant_dict:
+        list1 = plant_dict[key]
+        csv_writer.writerow([key[0]+'-'+key[1],list1[0], 1, "OP", min(list1[1],list1[2]),list1[3],list1[3],key[1]])
     plant.close()
 
-def Write_gen(clean_data):
+def Write_gen(plant_dict):
     gencost = open('gencost.csv','w',newline='')
     csv_writer = csv.writer(gencost)
     csv_writer.writerow(["plant_id","type","n","c2","c1","c0"])
-    for index, row in clean_data.iterrows():
-        csv_writer.writerow([row['PLANT']+'-'+row['NAME']+'-'+row['ZIP'],row['TYPE'],1,1,type_dict[row['TYPE']],0])
+    for key in plant_dict:
+        csv_writer.writerow([key[0]+'-'+key[1],key[1],1,1,type_dict[key[1]],0])
     gencost.close()
 
 def Plant(E_csv, U_csv, G2019_csv):
@@ -135,11 +151,16 @@ def Plant(E_csv, U_csv, G2019_csv):
     type_data = pd.read_csv("data/type.csv")
     for index, row in type_data.iterrows():
         type_dict[row['TYPE']] = row['Type_code']
+    plant_dict = Plant_agg(clean_data,ZipOfsub_dict,loc_of_plant,LocOfsub_dict,Pmin)
+    write_plant(plant_dict)
+    Write_gen(plant_dict)
 
-    write_plant(clean_data, ZipOfsub_dict)
-    Write_gen(clean_data)
-    
+
+
+
 Plant("data/Electric_Substations.csv", "data/General_Units.csv","data/Generator_Y2019.csv")
+
+
 
 
 
